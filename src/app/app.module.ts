fdd1a4529fe213028df5f5d32fdd4378b459bcb8
João Felipe <joao.yuki@gmail.com>
2018-12-22 16:16:15 -0200

    Adicinando uma diretiva *ngFor em nosso componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PhotosModule } from './photos/photos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
