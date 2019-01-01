dcd30fc05c9471f87053ad34862e228a42ddd0d0
João <joao.yuki@gmail.com>
2019-01-01 17:20:03 -0200

    Adicionado chamada a um servidor API com RxJS e observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  photos = [
+  photos: Object[] = [];
 
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
-      description: 'Leão'
-    },
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
-      description: 'Leoa'
-    }
+  constructor(private photoService: PhotoService) {
 
-  ];
+    photoService
+    .listFromUser('flavio')
+    .subscribe(photos => this.photos = photos);
 
+
+  }
 
 }
