01e5241b0a9acdf8b6d353ee18bc5300fa27bd76
João <joao.yuki@gmail.com>
2019-01-05 17:28:42 -0200

    Adicionando rotas e também um novo módulo para páginas inválidas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { ErrosModule } from './erros/erros.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    PhotosModule,
+    AppRoutingModule,
+    ErrosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
