e42a7496d9a9565137cae58b124bc64e26682487
João <joao.yuki@gmail.com>
2019-01-12 16:03:44 -0200

    Adicionado o uso de Subject para pegar a pesquisa de foto e aguardar

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { debounceTime } from 'rxjs/operators';
 
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
+
 
   photos: Photo[] = [];
   filter: string = 'Dado inicial';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(
     // private photoService: PhotoService,
@@ -27,7 +31,16 @@ export class PhotoListComponent implements OnInit {
     // .listFromUser(userName)
     // .subscribe(photos => this.photos = photos);
 
-    this.photos = this.activatedRoute.snapshot.data['photo'];
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter);
 
   }
+
+  //É chamado toda vez que um objeto é destruido
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
 }
