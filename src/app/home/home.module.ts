fff55291bf7687669688004f0f60a6a2007449d9
João <joao.yuki@gmail.com>
2019-01-14 21:35:00 -0200

    Assosciando um form no ts com um form no html e passando dados

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,13 @@
 import { SigninComponent } from './sigin/signin.component';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     SigninComponent
+  ],
+  imports: [
+    ReactiveFormsModule
   ]
 })
 export class HomeModule {
