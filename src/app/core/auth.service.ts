6f02761bbe21b65092f295a43e0d626006485fa5
João <joao.yuki@gmail.com>
2019-01-17 20:57:26 -0200

    Decodificando o token e emitindo o usuario que esta no token

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,4 +1,4 @@
-import { TokenService } from './token/token.service';
+import { UserService } from './user/user.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
@@ -12,7 +12,7 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private tokenService: TokenService
+    private userService: UserService
     ) { }
 
     authenticate(userName: string, password: string) {
@@ -21,7 +21,7 @@ export class AuthService {
        {observe: 'response'})
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
-        this.tokenService.setToken(authToken);
+        this.userService.setToken(authToken);
         console.log(authToken);
       }))
       ;
---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { TokenService } from '../token/token.service';
+import { Subject } from 'rxjs';
+import { User } from './user';
+import * as jwt_decode from 'jwt-decode';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+
+  private userSubject = new Subject<User>(); //Ele vai emitir um User e quem fizer o subscribe, vai receber um User
+
+  constructor(private tokenService: TokenService) {
+    //Esse serviço vai ser criado assim que eu entrar no sistema, antes mesmo de tentar logar
+    if (this.tokenService.hasToken() == true) {
+      this.decodeAndNotify();
+    }
+
+  }
+
+  setToken(token: string) {
+    this.tokenService.setToken(token);
+    this.decodeAndNotify();
+  }
+
+  getUser() { //Quem chamar esse método vai receber um subscribe
+    return this.userSubject.asObservable();
+  }
+
+  private decodeAndNotify() {
+    const token = this.tokenService.getToken();
+    const user = jwt_decode(token) as User; //Estou fazendo um cast para o tipo User
+    this.userSubject.next(user); //Aqui iremos emitir o User decodificado
+  }
+
+}
